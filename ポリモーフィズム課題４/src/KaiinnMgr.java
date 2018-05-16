eaf81aa471e864e1ae4beaaab795a65f43b008d0
eiichi hayashi <eiichi@mac.com>
2018-05-16 18:20:01 +0900

    s

---
+++ ポリモーフィズム課題４/src/KaiinnMgr.java
@@ -0,0 +1,145 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+//---------------------------------------------
+//会員管理
+public class KaiinnMgr extends Dao
+{
+	private static final String PUT_SQL =
+			"INSERT INTO kaiinn " +
+			"( " +
+			    "kaiinNo, "+
+				"name, " +
+				"registDate ) " +
+			"VALUES ( ?, ?, ? )";
+
+	private static final String ALLGET_SQL =
+		"select "
+		+ "kaiinNo, "
+		+ "name, "
+		+ "registDate "
+		+ "from "
+		+ " kaiinn";
+
+	private static final String GET_SQL =
+			"select "
+			+ "name, "
+			+ "registDate "
+			+ "from "
+			+ " kaiinn "
+			+ "where "
+			+ " kaiinNo = ? ";
+
+	public void put( Kaiinn k ) throws ExistException
+	{
+
+		try(
+				Connection con = Dao.getConnection();
+				PreparedStatement	stmt = con.prepareStatement(PUT_SQL);
+			)
+		{
+			if( this.get(k.getKaiinNo(),con) != null )
+				throw new ExistException();
+
+			/* Statementの作成 */
+			stmt.setInt(	1, k.getKaiinNo() 	);
+			stmt.setString(	2, k.getName()		);
+			stmt.setDate(	3, convertToSqlDate(k.getRegistDate()));
+
+			/* ｓｑｌ実行 */
+			int numCount = stmt.executeUpdate();
+		}
+		catch( ClassNotFoundException | SQLException e )
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Kaiinn get(int kaiinNo, Connection con)
+	{
+		Kaiinn k = null;
+		try(
+				PreparedStatement	stmt = con.prepareStatement(GET_SQL);
+			)
+		{
+			/* Statementの作成 */
+			stmt.setInt(	1, kaiinNo 	);
+
+			/* ｓｑｌ実行 */
+			ResultSet rset = stmt.executeQuery();
+
+
+			while(rset.next())
+			{
+				k = new Kaiinn(
+						kaiinNo,
+						rset.getString(1),
+						rset.getDate(2)
+						);
+			}
+		}
+		catch(SQLException e )
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return k;
+	}
+
+	//-------------------------------
+	public Kaiinn get( int kaiinNo )
+	{
+		Kaiinn k =null;
+		try(
+				Connection con = Dao.getConnection();
+			)
+		{
+			k =this.get( kaiinNo, con );
+		}
+		catch (ClassNotFoundException | SQLException e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return k;
+	}
+
+	public Collection<Kaiinn> values()
+	{
+		List<Kaiinn>  list = new ArrayList<Kaiinn>();
+
+		try(
+				Connection con = Dao.getConnection();
+				PreparedStatement	stmt = con.prepareStatement(ALLGET_SQL);
+			)
+		{
+			/* ｓｑｌ実行 */
+			ResultSet rset = stmt.executeQuery();
+
+
+			while(rset.next())
+			{
+				Kaiinn k = new Kaiinn(
+						rset.getInt(1),
+						rset.getString(2),
+						rset.getDate(3)
+						);
+
+				list.add(k);
+			}
+		}
+		catch( ClassNotFoundException | SQLException e )
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return list;
+	}
+}
--- ポリモーフィズム課題４/src/RegistMenuItem.java
+++ ポリモーフィズム課題４/src/RegistMenuItem.java
@@ -11,14 +11,21 @@ public class RegistMenuItem extends MenuItem
 
   public void excecAction() throws IOException
   {
-	  ConsoleUtil.print("会員を登録します。IDを入力してください。");
-      int id = ConsoleUtil.inputIntFromConsole();
+	ConsoleUtil.print("会員を登録します。IDを入力してください。");
+    int id = ConsoleUtil.inputIntFromConsole();
 
-      ConsoleUtil.print("名前を入力してください。");
-      String name = ConsoleUtil.inputStringFromConsole();
+    ConsoleUtil.print("名前を入力してください。");
+    String name = ConsoleUtil.inputStringFromConsole();
 
-      Kaiinn k = new Kaiinn(id, name );
-      this.kaiinnMgr.put(k);
+    Kaiinn k = new Kaiinn(id, name );
+    try
+    {
+		this.kaiinnMgr.put(k);
+	}
+    catch (ExistException e)
+    {
+    	ConsoleUtil.print("ご指定のIDはすでに使われています。");
+    }
       ConsoleUtil.print("登録が完了しました。:" + k.toString());
   }
 }
